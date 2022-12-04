e57589da252444c8003a7a02573c2a3d83daf090
Håkon Lie <hakon.lie@vipps.no>
2022-12-04 09:59:17 +0100

    day3

--- 03/main.go
+++ 03/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"fmt"
+	"strings"
 )
 
 //go:embed *
@@ -10,13 +11,49 @@ var f embed.FS
 
 func main() {
 	input, _ := f.ReadFile("input")
-	fmt.Println(p1)
+	lines := strings.Split(string(input), "\n")
+	fmt.Println(p2(lines))
 }
 
-func p1(string input) {
-
+func getPrio(r rune) int {
+	var prio int
+	if int(r) <= 95 {
+		prio = (int(r) % 65) + 27
+	} else {
+		prio = (int(r) % 97) + 1
+	}
+	return prio
 }
 
-func p2(string input) {
+func p1(lines []string) int {
+	ans := 0
+lines:
+	for _, line := range lines {
+		compartmentOne := []rune(line)[:len(line)/2]
+		compartmentTwo := string([]rune(line)[len(line)/2:])
+		for _, r := range compartmentOne {
+			if strings.ContainsRune(compartmentTwo, r) {
+				ans += getPrio(r)
+				continue lines
+			}
+		}
+	}
+	return ans
+}
 
+func p2(lines []string) int {
+	ans := 0
+lines:
+	for i := 0; i <= len(lines)-3; i += 3 {
+		elfOne := lines[i]
+		elfTwo := lines[i+1]
+		elfThree := lines[i+2]
+		for _, r := range []rune(elfOne) {
+			if strings.ContainsRune(elfTwo, r) && strings.ContainsRune(elfThree, r) {
+				ans += getPrio(r)
+				continue lines
+			}
+		}
+	}
+	return ans
 }
--- 03/readme.md
+++ 03/readme.md
@@ -1,3 +1,6 @@
 ## Day 3
 
-### First solution
+### Solution
+I feel smart when I work with runes.
+This was easier than I thought tbh, when I first looked at it.
+Just had to figure out the conversion from rune to priority.
