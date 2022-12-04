16571e29804f5565c93b75e01bf569c7a3eed533
Håkon Lie <hakon.lie@vipps.no>
2022-12-04 10:42:03 +0100

    day4

---
+++ 04/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed *
+var f embed.FS
+
+func main() {
+	input, _ := f.ReadFile("input")
+	lines := strings.Split(string(input), "\n")
+	fmt.Println(p2(lines))
+}
+
+func convertDisregardError(n string) int {
+	i, _ := strconv.Atoi(n)
+	return i
+}
+
+func p1(lines []string) int {
+	ans := 0
+	for _, l := range lines {
+		pair := strings.Split(l, ",")
+		elfOne := strings.Split(pair[0], "-")
+		elfTwo := strings.Split(pair[1], "-")
+
+		// pair: [55-75 9-55] is: true
+		overlapping := false
+		if convertDisregardError(elfOne[0]) >= convertDisregardError(elfTwo[0]) && convertDisregardError(elfOne[1]) <= convertDisregardError(elfTwo[1]) {
+			overlapping = true
+		} else if convertDisregardError(elfTwo[0]) >= convertDisregardError(elfOne[0]) && convertDisregardError(elfTwo[1]) <= convertDisregardError(elfOne[1]) {
+			overlapping = true
+		}
+		if overlapping {
+			ans++
+		}
+	}
+	return ans
+}
+
+func p2(lines []string) int {
+	ans := 0
+	for _, l := range lines {
+		pair := strings.Split(l, ",")
+		elfOne := strings.Split(pair[0], "-")
+		elfTwo := strings.Split(pair[1], "-")
+
+		elfOne1 := convertDisregardError(elfOne[0])
+		elfOne2 := convertDisregardError(elfOne[1])
+		elfTwo1 := convertDisregardError(elfTwo[0])
+		elfTwo2 := convertDisregardError(elfTwo[1])
+
+		// pair: [31-94 30-39] is: false
+		overlapping := false
+		if elfOne1 >= elfTwo1 && elfOne1 <= elfTwo2 {
+			overlapping = true
+		}
+		if elfOne2 >= elfTwo1 && elfOne2 <= elfTwo2 {
+			overlapping = true
+		}
+		if elfOne1 >= elfTwo1 && elfOne2 <= elfTwo2 {
+			overlapping = true
+		}
+		if elfTwo1 >= elfOne1 && elfTwo2 <= elfOne2 {
+			overlapping = true
+		}
+		fmt.Printf("pair: %s is overlapping: %v \n", pair, overlapping)
+
+		if overlapping {
+			ans++
+		}
+	}
+
+	return ans
+
+}
---
+++ 04/readme.md
@@ -0,0 +1,6 @@
+## Day 
+
+### Solution
+Ugh, forgot to convert to number for a while and couldnt understand why the matching didnt make sense.
+Not in love with this solution, might try to make something fancier if I come up with anything later.
+
